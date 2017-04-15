557cd3cf8907d6d6e6cae333f1f4321e79734960
Adam <acdoyle630@gmail.com>
2017-04-14 19:41:24 -1000

    html and css happy path

--- server.js
+++ server.js
@@ -1,13 +1,33 @@
 /*jshint esversion: 6*/
 
 const http = require('http');
+const fs = require('fs');
+
+
+
 
 const server = http.createServer((req, res)  => {
-  console.log(req.method);
-  res.end('ended');
 
+  let method = req.method;
+  let url = req.url;
+  let host = req.host;
+  if(url ==='/' || url ===''){
+    url = 'index.html';
+  }
+  console.log(url);
+
+
+  if(method === 'GET'){
+  var fileName = fs.readFile(`./public/${url}`,(err, data) => {
+    //if (err) throw err;
+    res.writeHead(200);
+    res.end(data);
+    });
+  }
 });
+
 
 server.listen(6969, () => {
   console.log('server');
 });
+
